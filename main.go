80a1366098a75f0e41a7e009da4b37269874c3b6
pedromol <pedro@mol.net.br>
2023-05-04 19:11:53 -0300

    fix memory allocation

--- main.go
+++ main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-    "math/rand"
+	"bufio"
 	"crypto/aes"
+
 	"encoding/hex"
-	"runtime"
-	"os"
-    "strconv"
+	"errors"
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var l = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 func rnd(n int) string {
-    s := make([]rune, n)
-    for i := range s {
-        s[i] = l[rand.Intn(len(l))]
-    }
-    return string(s)
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = l[rand.Intn(len(l))]
+	}
+	return string(s)
 }
 
 func encrypt(k string, m string) string {
 	c, _ := aes.NewCipher([]byte(k))
-	
+
 	msg := make([]byte, len(m))
 	c.Encrypt(msg, []byte(m))
 	return hex.EncodeToString(msg)
@@ -31,35 +34,67 @@ func encrypt(k string, m string) string {
 func decrypt(k string, m string) string {
 	txt, _ := hex.DecodeString(m)
 	c, _ := aes.NewCipher([]byte(k))
-	
+
 	msg := make([]byte, len(txt))
 	c.Decrypt(msg, []byte(txt))
 
 	return string(msg[:])
 }
 
+func readMemoryTotal() (int, error) {
+	file, err := os.Open("/proc/meminfo")
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		text := strings.ReplaceAll(line[:len(line)-2], " ", "")
+		keyValue := strings.Split(text, ":")
+
+		switch keyValue[0] {
+		case "MemTotal":
+			value, err := strconv.Atoi(keyValue[1])
+			if err != nil {
+				return 0, err
+			}
+			return value, nil
+		}
+	}
+	return 0, errors.New("unable to read total memory size")
+}
+
 func main() {
-	var data = make([]byte, 1)
-    memPercentageStr := os.Getenv("MEMORY_PERCENTAGE")
-    if memPercentageStr != "" {
+	var memToAllocate uint64
+	var data = make([]byte, 0)
+
+	memPercentageStr := os.Getenv("MEMORY_PERCENTAGE")
+	if memPercentageStr != "" {
 		memPercentage, err := strconv.ParseFloat(memPercentageStr, 64)
 		if err != nil {
-			fmt.Println("Invalid memory percentage:", memPercentageStr)
+			fmt.Println("Invalid memory percentage:", err)
 			os.Exit(1)
 		}
-		fmt.Println("Burning % memory:", memPercentageStr)
-	
-		mem := runtime.MemStats{}
-		runtime.ReadMemStats(&mem)
-		totalMem := mem.Sys
-	
-		memToAllocate := uint64(float64(totalMem) * memPercentage / 100)
-	
-		data = make([]byte, memToAllocate)
-		data[0] = 1
+
+		totalMem, err := readMemoryTotal()
+		if err != nil {
+			fmt.Println("Failed to read total memory:", err)
+			os.Exit(1)
+		}
+
+		memToAllocate = uint64(float64(totalMem) * memPercentage / 100)
+		data = make([]byte, memToAllocate*1000)
+
+		fmt.Printf("Allocated %.2f MB of memory (%.2f%% of total memory)\n", float64(memToAllocate)/1000, memPercentage)
+		fmt.Printf("Total memory: %.2f MB\n", float64(totalMem)/1000)
+
 	}
 	for {
-		key := rnd(32)
-		decrypt(key, encrypt(key, key))
+		for i := range data {
+			data[i] = byte([]byte(rnd(1))[0])
+			key := rnd(32)
+			decrypt(key, encrypt(key, key))
+		}
 	}
 }
